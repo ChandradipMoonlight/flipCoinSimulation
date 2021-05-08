5c8d5882d9a64aecf2fdf36308641cd71b905ae4
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-08 22:15:51 +0530

    coin flip multiple times

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,11 +1,15 @@
 #! /bin/bash -x
 echo "Wellcome to flip coin simulation program"
 
-flipCoin=$((RANDOM%2))
+for ((i=0; i<21; i++))
+do
 
-if [ $flipCoin -eq 1 ]
-then
-	echo Head
-else
-	echo Tail
-fi
+	flipCoin=$((RANDOM%2))
+
+	if [ $flipCoin -eq 1 ]
+	then
+		echo "Head "
+	else
+		echo "Tail "
+	fi
+done
